660cd95dcea72789d93d1ba0a431fadad759fe74
John W Granados A - Master Mintaka <jwga76@gmail.com>
2025-01-07 21:54:50 -0500

    Database and config files to separate responsibilities

---
+++ Database.php
@@ -0,0 +1,22 @@
+<?php
+
+class Database
+{
+    public $connection;
+
+    public function __construct()
+    {
+        $config = require 'config.php';
+
+        $dsn = "mysql:" . http_build_query($config['database'], '', ';');
+        $options = [PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC];
+        $this->connection = new PDO($dsn, $config['database']['dbuser'], $config['database']['dbpwd'], $options);
+    }
+
+    public function query($query): bool|PDOStatement
+    {
+        $statement = $this->connection->prepare($query);
+        $statement->execute();
+        return $statement;
+    }
+}
---
+++ config.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    'database' => [
+        'host' => "localhost",
+        'port' => 3306,
+        'dbname' => "demo_php",
+        'charset' => "utf8mb4",
+        'dbuser' => 'root',
+        'dbpwd' => 'WsxOkn1732.*'
+    ]
+];
