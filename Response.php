fe58a5c51f28214137b5a43b2099f395350e1146
John W Granados A - Master Mintaka <jwga76@gmail.com>
2025-01-10 23:47:55 -0500

    Complete Response with other http status code constants

--- Response.php
+++ Response.php
@@ -3,9 +3,16 @@
 class Response
 {
     const HTTP_OK = 200;
-    const HTTP_NOT_FOUND = 404;
-    const HTTP_INTERNAL_SERVER_ERROR = 500;
+    const HTTP_CREATED = 201;
+    const HTTP_NO_CONTENT = 204;
     const HTTP_BAD_REQUEST = 400;
     const HTTP_UNAUTHORIZED = 401;
     const HTTP_FORBIDDEN = 403;
+    const HTTP_NOT_FOUND = 404;
+    const HTTP_METHOD_NOT_ALLOWED = 405;
+    const HTTP_INTERNAL_SERVER_ERROR = 500;
+    const HTTP_NOT_IMPLEMENTED = 501;
+    const HTTP_BAD_GATEWAY = 502;
+    const HTTP_SERVICE_UNAVAILABLE = 503;
+    const HTTP_GATEWAY_TIMEOUT = 504;
 }
