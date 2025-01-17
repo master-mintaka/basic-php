f90b81154847048267ce930e6a97742df23233b4
John W Granados A - Master Mintaka <jwga76@gmail.com>
2025-01-16 22:43:28 -0500

    Add validation for note creation and improve error handling

--- controllers/note-create.php
+++ controllers/note-create.php
@@ -1,18 +1,32 @@
 <?php
 $heading = "Create Note";
 
-if($_SERVER['REQUEST_METHOD'] === 'POST') {
-    
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+
     $db = Database::getInstance(require 'config.php');
-    $db->query("INSERT INTO notes (body, user_id) VALUES (:body, :user_id)", [
-        'body' => $_POST['body'],
-        'user_id' => 1
-    ]);
-    $db->close();
-    header('Location: /notes');
-    
 
+    //VALIDATION
+    $errors = [];
+    $content = [];
+
+    if (!isset($_POST['body']) || empty($_POST['body'])) {
+        $errors['body'] = 'The body field is required';
+    }
+
+    if (strlen($_POST['body']) > 500) {
+        $errors['body'] = 'The body field must be less than 500 characters';
+        $content['body'] = $_POST['body'];
+    }
+
+    if (empty($errors)) {
+        $query = "INSERT INTO notes (body, user_id) VALUES (:body, :user_id)";
+        $db->query($query, [
+            'body' => $_POST['body'],
+            'user_id' => 1
+        ]);
+        $db->close();
+        header('Location: /notes');
+    }
 }
-
 
 require 'views/note-create.view.php';
--- views/note-create.view.php
+++ views/note-create.view.php
@@ -2,6 +2,8 @@
 <?php require "partials/nav.php" ?>
 <?php require "partials/banner.php" ?>
 
+
+
 <main>
     <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
         <!-- Your content -->
@@ -12,8 +14,15 @@
                     <div class="col-span-full">
                         <label for="body" class="block text-sm/6 font-medium text-gray-900">Digita el texto</label>
                         <div class="mt-2">
-                            <textarea name="body" id="body" rows="3" class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"></textarea>
+                            <textarea
+                                name="body"
+                                id="body"
+                                rows="5"
+                                class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"><?= $content['body'] ?? '' ?></textarea>
                         </div>
+                        <?php if (isset($errors['body'])) : ?>
+                            <p class="mt-2 text-sm/6 text-red-600"><?= $errors['body'] ?></p>
+                        <?php endif; ?>
                     </div>
                 </div>
             </div>
